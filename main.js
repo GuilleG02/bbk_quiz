c34b8a6b3f598a5a95134b4e385c21e401fd3354
GuilleG02 <guille.g0220rgmail.com>
2025-05-01 15:42:20 +0200

    Cambio para recoger preguntas de la API y Css simple

--- main.js
+++ main.js
@@ -5,129 +5,127 @@ const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question');
 const answerButtonsElement = document.getElementById('answer-buttons');
 
+let currentQuestionIndex = 0;
+let questionList = [];
+
+const API_URL = 'https://opentdb.com/api.php?amount=10&type=multiple';
+
+async function loadQuestionsFromAPI() {
+
+  try {
+
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    questionList = data.results.map(item => {
+      const allAnswers = [...item.incorrect_answers, item.correct_answer];
+      const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5);
+      return {
+
+        question: decodeHTML(item.question),
+        answers: shuffledAnswers.map(answer => ({
+          text: decodeHTML(answer),
+          correct: answer === item.correct_answer
+        }))
+
+      };
+    });
+
+    startGame();
+  } catch (error) {
+    console.error('Error cargando preguntas:', error);
+  }
+}
 
 
-let currentQuestionIndex
+function decodeHTML(html) {
+
+  const txt = document.createElement('textarea');
+  txt.innerHTML = html;
+  return txt.value;
+}
 
 
 function startGame() {
- startButton.classList.add('hide')
- currentQuestionIndex = 0
- questionContainerElement.classList.remove('hide')
+
+  startButton.classList.add('hide');
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
 }
 
-startButton.addEventListener('click', startGame)
+
+function setNextQuestion() {
+
+  resetState();
+  showQuestion(questionList[currentQuestionIndex]);
+}
 
 
-const questionList = [
-    {
-      question: 'What is 2 + 2?',
-      answers: [
-        { text: '4', correct: true },
-        { text: '22', correct: false },
-      ],
-    },
-    {
-      question: 'Is web development fun?',
-      answers: [
-        { text: 'Kinda', correct: false },
-        { text: 'YES!!!', correct: true },
-        { text: 'Um no', correct: false },
-        { text: 'IDK', correct: false },
-      ],
-    },
-    {
-      question: 'What is 4 * 2?',
-      answers: [
-        { text: '6', correct: false },
-        { text: '8', correct: true },
-        { text: 'Yes', correct: false },
-      ],
-    },
-   ]
+function resetState() {
 
-   
-   function showQuestion(item) {
-    questionElement.innerText = item.question
-    item.answers.forEach((answer) => {
-      const button = document.createElement('button')
-      button.innerText = answer.text
-   
-      if (answer.correct) {
-        button.dataset.correct = true
-      }
-   
-   
-      answerButtonsElement.appendChild(button)
-    })
-   }
-
-   
-   function setNextQuestion() {
-    showQuestion(questionList[currentQuestionIndex])
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
+}
 
-  function startGame() {
-    startButton.classList.add('hide')
-    currentQuestionIndex = 0
-    questionContainerElement
-   .classList
-   .remove('hide')
-    setNextQuestion()
-   }
-   
 
-   function setStatusClass(element) {
-    if (element.dataset.correct) {
-      element.classList.add('color-correct')
-    } else {
-      element.classList.add('color-wrong')
+function showQuestion(question) {
+
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+
+    if (answer.correct) {
+
+      button.dataset.correct = true;
     }
-   }
 
-   
-   function selectAnswer() {
-    Array
-   .from(answerButtonsElement.children)
-   .forEach((button) => { setStatusClass(button) })
-    if (questionList.length > currentQuestionIndex + 1) {
-      nextButton.classList.remove('hide')
-    } else {
-      startButton.innerText = 'Restart'
-      startButton.classList.remove('hide')
-    }
-   }
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
 
-   function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach((answer) => {
-      const button = document.createElement('button')
-      button.innerText = answer.text
-      if (answer.correct) {
-        button.dataset.correct = true
-      }
-      button.addEventListener('click', selectAnswer)
-      answerButtonsElement.appendChild(button)
-    })
-   }
-   
-   nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
- })
 
- 
- function resetState() {
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-      answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    }
-   }
+function selectAnswer(e) {
 
-   function setNextQuestion() {
-    resetState()
-    showQuestion(questionList[currentQuestionIndex])
+  const selectedButton = e.target;
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button);
+  });
+
+  if (questionList.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide');
+
+  } else {
+
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
   }
-  
+}
+
+
+function setStatusClass(element) {
+
+  if (element.dataset.correct) {
+    element.classList.add('color-correct');
+  } else {
+
+    element.classList.add('color-wrong');
+  }
+}
+
+
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+startButton.addEventListener('click', () => {
+  loadQuestionsFromAPI(); 
+});
+
 
 
